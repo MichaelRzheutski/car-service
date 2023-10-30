da9cf1e365844cb07c07d76396d34168ccf1ceb0
MichaelRzheutski <michael.rzheutski@gmail.com>
2023-10-30 21:58:22 +0300

    Create CarService class

---
+++ src/CarService.java
@@ -0,0 +1,19 @@
+// CarService: Provides a name of the car service
+public class CarService {
+    private String carServiceName;
+
+    public CarService(){};
+
+    public CarService(String carServiceName){
+        this.carServiceName = carServiceName;
+    }
+
+    public String getCarServiceName() {
+        return carServiceName;
+    }
+
+    public void setCarServiceName(String carServiceName) {
+        this.carServiceName = carServiceName;
+    }
+
+}
