e69f2a85f84548330c27c52e69d95673df97f159
MichaelRzheutski <michael.rzheutski@gmail.com>
2023-10-30 21:56:52 +0300

    Create Appointment class

---
+++ src/Appointment.java
@@ -0,0 +1,16 @@
+// Appointment: Represents a date of appointment.
+public class Appointment {
+    private String date;
+
+    public Appointment(String date) {
+        this.date = date;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
