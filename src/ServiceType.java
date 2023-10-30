0e1df3e65d5d8e0a4d8a2c02496954e4f14d3ffa
MichaelRzheutski <michael.rzheutski@gmail.com>
2023-10-30 22:03:28 +0300

    Create ServiceType class

---
+++ src/ServiceType.java
@@ -0,0 +1,43 @@
+// ServiceType: Represents service type and makes of parts it using in process.
+public class ServiceType extends CarService {
+    private String serviceType;
+    private String makeOfOil;
+    private String makeOfTires;
+    private String makeOfBrakes;
+
+    public ServiceType(String serviceType) {
+        this.serviceType = serviceType;
+    }
+
+    public String getServiceType() {
+        return serviceType;
+    }
+
+    public void setServiceType(String serviceType) {
+        this.serviceType = serviceType;
+    }
+
+    public String getMakeOfOil() {
+        return makeOfOil;
+    }
+
+    public void setMakeOfOil(String makeOfOil) {
+        this.makeOfOil = makeOfOil;
+    }
+
+    public String getMakeOfTires() {
+        return makeOfTires;
+    }
+
+    public void setMakeOfTires(String makeOfTires) {
+        this.makeOfTires = makeOfTires;
+    }
+
+    public String getMakeOfBrakes() {
+        return makeOfBrakes;
+    }
+
+    public void setMakeOfBrakes(String makeOfBrakes) {
+        this.makeOfBrakes = makeOfBrakes;
+    }
+}
