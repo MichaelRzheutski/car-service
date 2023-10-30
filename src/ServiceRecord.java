6599da947484c065732528e9d7b52ff5aebb2a44
MichaelRzheutski <michael.rzheutski@gmail.com>
2023-10-30 22:02:49 +0300

    Create ServiceRecord class

---
+++ src/ServiceRecord.java
@@ -0,0 +1,36 @@
+// ServiceRecord: Represents provided service, date and cost
+public class ServiceRecord extends CarService {
+    private ServiceType serviceProvided;
+    private String date;
+    private double cost;
+
+    public ServiceRecord(ServiceType serviceProvided, String date, double cost) {
+        this.serviceProvided = serviceProvided;
+        this.date = date;
+        this.cost = cost;
+    }
+
+    public ServiceType getServiceProvided() {
+        return serviceProvided;
+    }
+
+    public void setServiceProvided(ServiceType serviceProvided) {
+        this.serviceProvided = serviceProvided;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+}
