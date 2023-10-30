87c609468d7fff328dae2e40786af5979de136e7
MichaelRzheutski <michael.rzheutski@gmail.com>
2023-10-30 22:04:48 +0300

    Create SparePartsShop class

---
+++ src/SparePartsShop.java
@@ -0,0 +1,46 @@
+import java.util.Formatter;
+import java.util.Locale;
+
+// SparePartsShop: Represents the name and cost of the spare part which on sale
+public class SparePartsShop extends CarService {
+    private SparePart sparePartName;
+    private double sparePartCost;
+
+    public SparePartsShop(SparePart sparePartName, double sparePartCost) {
+        this.sparePartName = sparePartName;
+        this.sparePartCost = sparePartCost;
+    }
+
+    public static void showSparePartsInStore(SparePartsShop[] sparePartsInShop) {
+        for (SparePartsShop spares : sparePartsInShop) {
+            if (spares.getSparePartName().isInStock()) {
+                Formatter sparePartFormat = new Formatter(Locale.US).format(
+                        "Запчасть '%s' в наличии в магазине по цене %.2f\n",
+                        spares.getSparePartName().getSparePartName(),
+                        spares.getSparePartCost());
+                System.out.printf(sparePartFormat.toString());
+            } else {
+                System.out.printf(
+                        "Запчасть '%s' отсутствует в магазине\n",
+                        spares.getSparePartName().getSparePartName()
+                );
+            }
+        }
+    }
+
+    public SparePart getSparePartName() {
+        return sparePartName;
+    }
+
+    public void setSparePartName(SparePart sparePartName) {
+        this.sparePartName = sparePartName;
+    }
+
+    public double getSparePartCost() {
+        return sparePartCost;
+    }
+
+    public void setSparePartCost(double sparePartCost) {
+        this.sparePartCost = sparePartCost;
+    }
+}
