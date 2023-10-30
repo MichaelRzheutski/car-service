1d2b5058081663b67c0242e70b5472c9a943893e
MichaelRzheutski <michael.rzheutski@gmail.com>
2023-10-30 22:03:59 +0300

    Create SparePart class

---
+++ src/SparePart.java
@@ -0,0 +1,26 @@
+// SparePart: Represents the spare part name and availability in stock
+public class SparePart extends CarService {
+    private String sparePartName;
+    private boolean isInStock;
+
+    public SparePart(String sparePartName, boolean isInStock) {
+        this.sparePartName = sparePartName;
+        this.isInStock = isInStock;
+    }
+
+    public String getSparePartName() {
+        return sparePartName;
+    }
+
+    public void setSparePartName(String sparePart) {
+        this.sparePartName = sparePart;
+    }
+
+    public boolean isInStock() {
+        return isInStock;
+    }
+
+    public void setInStock(boolean inStock) {
+        isInStock = inStock;
+    }
+}
