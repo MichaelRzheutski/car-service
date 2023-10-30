4ca396ccb40241137d60f3e527980d0ff907224d
MichaelRzheutski <michael.rzheutski@gmail.com>
2023-10-30 22:02:11 +0300

    Create Person class

---
+++ src/Person.java
@@ -0,0 +1,26 @@
+// Person: Represents an abstract person
+public abstract class Person {
+    protected String name;
+    protected String surname;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
