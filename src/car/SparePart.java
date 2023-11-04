c1ab782bcaae313c858155295bd932ad2a648f61
MichaelRzheutski <michael.rzheutski@gmail.com>
2023-11-04 03:58:31 +0300

    Override equals(), hashCode(), toString in SparePart class

--- src/car/SparePart.java
+++ src/car/SparePart.java
@@ -1,21 +1,36 @@
 package car;
 
+import helpers.spareparts.SparePartMakes;
+import helpers.spareparts.SparePartTypes;
+
+import java.util.Objects;
+
 // SparePart: Represents the spare part name and availability in stock
-public class SparePart {
-    private String sparePartName;
+public class SparePart extends Car {
+    private SparePartTypes sparePartType;
+    private SparePartMakes sparePartMake;
     private boolean isInStock;
 
-    public SparePart(String sparePartName, boolean isInStock) {
-        this.sparePartName = sparePartName;
+    public SparePart(SparePartTypes sparePartType, SparePartMakes sparePartMake, boolean isInStock) {
+        this.sparePartType = sparePartType;
+        this.sparePartMake = sparePartMake;
         this.isInStock = isInStock;
     }
 
-    public String getSparePartName() {
-        return sparePartName;
+    public SparePartTypes getSparePartType() {
+        return sparePartType;
     }
 
-    public void setSparePartName(String sparePart) {
-        this.sparePartName = sparePart;
+    public void setSparePartType(SparePartTypes sparePart) {
+        this.sparePartType = sparePart;
+    }
+
+    public SparePartMakes getSparePartMake() {
+        return sparePartMake;
+    }
+
+    public void setSparePartMake(SparePartMakes sparePartMake) {
+        this.sparePartMake = sparePartMake;
     }
 
     public boolean isInStock() {
@@ -24,5 +39,28 @@ public class SparePart {
 
     public void setInStock(boolean inStock) {
         isInStock = inStock;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        SparePart sparePart = (SparePart) o;
+        return isInStock == sparePart.isInStock
+                && Objects.equals(sparePartType, sparePart.sparePartType)
+                && Objects.equals(sparePartMake, sparePart.sparePartMake);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), sparePartType, sparePartMake, isInStock);
+    }
+
+    @Override
+    public String toString() {
+        return "Тип запчасти: " + sparePartType + ", " +
+                " Марка запчасти: " + sparePartMake + ", " +
+                " Наличие на складе: " + isInStock;
     }
 }
