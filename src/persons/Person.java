9c8695149cc80d7610e7d1c18ea5f0584665c4b4
MichaelRzheutski <michael.rzheutski@gmail.com>
2023-11-04 03:57:43 +0300

    Override equals(), hashCode(), toString in Person class

--- src/persons/Person.java
+++ src/persons/Person.java
@@ -1,13 +1,22 @@
 package persons;
 
+import java.util.Objects;
+
 // Person: Represents an abstract person
 public abstract class Person {
     protected String name;
     protected String surname;
+    protected String address;
 
     public Person(String name, String surname) {
         this.name = name;
         this.surname = surname;
+    }
+
+    public Person(String name, String surname, String address) {
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
     }
 
     public String getName() {
@@ -24,5 +33,38 @@ public abstract class Person {
 
     public void setSurname(String surname) {
         this.surname = surname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(name, person.name)
+                && Objects.equals(surname, person.surname)
+                && Objects.equals(address, person.address
+        );
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, address);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", address='" + address + '\'' +
+                '}';
     }
 }
