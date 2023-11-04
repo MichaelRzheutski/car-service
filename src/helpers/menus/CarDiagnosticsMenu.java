747eb55011e95c3ad08358289823ed14a2761481
MichaelRzheutski <michael.rzheutski@gmail.com>
2023-11-04 03:43:02 +0300

    Add CarDiagnosticsMenu class

---
+++ src/helpers/menus/CarDiagnosticsMenu.java
@@ -0,0 +1,37 @@
+package helpers.menus;
+
+import helpers.ObjectsCreator;
+
+import java.util.Scanner;
+
+import static car.CarDiagnostics.checkCar;
+import static helpers.ConsoleColors.*;
+
+public class CarDiagnosticsMenu {
+    private static final ObjectsCreator OBJECTS_CREATOR = new ObjectsCreator();
+
+    // Car diagnostics menu
+    public static void diagnosticsServiceMenu(Scanner scanner, boolean exit) {
+        System.out.printf("%sВыберите авто для диагностики%s\n", ANSI_GREEN, ANSI_RESET);
+        System.out.println("[1]. Диагностика BMW X6");
+        System.out.println("[2]. Диагностика Toyota LandCruiser");
+        System.out.println("[3]. Диагностика Mercedes Benz");
+        System.out.println("[0]. Выход");
+
+        int option = scanner.nextInt();
+        scanner.nextLine();
+
+        while (!exit) {
+            switch (option) {
+                case 0 -> exit = true;
+                case 1 -> checkCar(OBJECTS_CREATOR.bmwX6Diagnostics);
+                case 2 -> checkCar(OBJECTS_CREATOR.toyotaLandCruiserDiagnostics);
+                case 3 -> checkCar(OBJECTS_CREATOR.mercedesBenzDiagnostics);
+                default -> System.out.printf(
+                        "%sНеверная операция, попробуйте ещё раз!%s\n", ANSI_RED, ANSI_RESET
+                );
+            }
+            break;
+        }
+    }
+}
