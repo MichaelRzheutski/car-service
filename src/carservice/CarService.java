b424785f3bf9a4078f4ad27640cc1aa259787115
MichaelRzheutski <michael.rzheutski@gmail.com>
2023-11-04 03:53:06 +0300

    left only constructor in CarService class

--- src/carservice/CarService.java
+++ src/carservice/CarService.java
@@ -1,21 +1,7 @@
 package carservice;
 
-// CarService: Provides a name of the car service
-public class CarService {
-    private String carServiceName;
-
-    public CarService(){};
-
-    public CarService(String carServiceName){
-        this.carServiceName = carServiceName;
+// CarService: Abstract class representing CarService
+public abstract class CarService {
+    public CarService() {
     }
-
-    public String getCarServiceName() {
-        return carServiceName;
-    }
-
-    public void setCarServiceName(String carServiceName) {
-        this.carServiceName = carServiceName;
-    }
-
 }
