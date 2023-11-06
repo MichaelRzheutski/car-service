760d46f2db87407b3c828f8daf014b999bcc3ab1
MichaelRzheutski <michael.rzheutski@gmail.com>
2023-11-06 03:32:32 +0300

    Modify RepairmentTimeCalculator class

--- src/helpers/calculators/RepairmentTimeCalculator.java
+++ src/helpers/calculators/RepairmentTimeCalculator.java
@@ -1,6 +1,8 @@
 package helpers.calculators;
 
+import car.SparePart;
 import helpers.ObjectsCreator;
+import persons.Mechanic;
 
 import java.util.Scanner;
 
@@ -29,17 +31,23 @@ public class RepairmentTimeCalculator {
                     break;
                 case 1:
                     repairmentTimeCalculator(
-                            OBJECTS_CREATOR.bmwX6Diagnostics.getDiagnosticsTime()
+                            OBJECTS_CREATOR.bmwX6Diagnostics.getDiagnosticsTime(),
+                            OBJECTS_CREATOR.bmwX6.getSpareParts(),
+                            OBJECTS_CREATOR.mechanics
                     );
                     break;
                 case 2:
                     repairmentTimeCalculator(
-                            OBJECTS_CREATOR.toyotaLandCruiserDiagnostics.getDiagnosticsTime()
+                            OBJECTS_CREATOR.toyotaLandCruiserDiagnostics.getDiagnosticsTime(),
+                            OBJECTS_CREATOR.toyotaLandCruiser.getSpareParts(),
+                            OBJECTS_CREATOR.mechanics
                     );
                     break;
                 case 3:
                     repairmentTimeCalculator(
-                            OBJECTS_CREATOR.mercedesBenzDiagnostics.getDiagnosticsTime()
+                            OBJECTS_CREATOR.mercedesBenzDiagnostics.getDiagnosticsTime(),
+                            OBJECTS_CREATOR.mercedesBenz.getSpareParts(),
+                            OBJECTS_CREATOR.mechanics
                     );
                     break;
                 default:
@@ -52,9 +60,10 @@ public class RepairmentTimeCalculator {
         }
     }
 
-    // Method calculates total cost of repairment
+    // Method calculates total cost of repairment including diagnostics result,
+    // damages severity and term of spare parts delivery
     public static void repairmentTimeCalculator(
-            int diagnosticsTime
+            int diagnosticsTime, SparePart[] spareParts, Mechanic[] mechanics
     ) {
         RepairmentCostCalculator.checkDiagnosticsResult(
                 OBJECTS_CREATOR.bmwX6Diagnostics.getDiagnosticsResult(),
@@ -72,8 +81,33 @@ public class RepairmentTimeCalculator {
                 OBJECTS_CREATOR.bmwX6Diagnostics.getDiagnosticsTime()
         );
 
+        int totalRepairmentTime = 0;
+        int sparePartsDeliveryTime;
+        for (SparePart sparePart : spareParts) {
+            sparePartsDeliveryTime = sparePart.getDeliveryDays();
+            totalRepairmentTime = diagnosticsTime + sparePartsDeliveryTime;
+        }
+
+        String mechanicExpertise;
+        for (Mechanic mechanic : mechanics) {
+            mechanicExpertise = mechanic.getExpertise();
+
+            if (mechanicExpertise.equals("мастер")) {
+                totalRepairmentTime += 1;
+                break;
+            }
+            if (mechanicExpertise.equals("специалист")) {
+                totalRepairmentTime += 2;
+                break;
+            }
+            if (mechanicExpertise.equals("стажёр")) {
+                totalRepairmentTime += 3;
+                break;
+            }
+        }
+
         System.out.println(ANSI_GREEN + "Общее время ремонта автомобиля в днях: "
-                + ANSI_YELLOW + diagnosticsTime + ANSI_RESET
+                + ANSI_YELLOW + totalRepairmentTime + ANSI_RESET
         );
         System.out.println();
     }
